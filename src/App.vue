f79f2b8243e61cf1ad032ffea63d03f302c88a3e
angsuo <angsuo@13l>
2021-08-06 16:00:14 +0200

    reactive is more sensitive to changes than vue 2's Object.defineProperty()

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,13 @@
   <ul>
     <li v-for="(hd, index) in infos.hobbies.day" :key="index">{{ hd }}</li>
   </ul>
-  <h2>During night, if I feel happy, I'm {{ infos.hobbies.night.happy }}</h2>
-  <h2>During night, if I feel sad, I'm {{ infos.hobbies.night.sad }}</h2>
+  <div v-if="infos.hobbies.night">
+  <h2 v-show="infos.hobbies.night">During night, if I feel happy, I'm {{ infos.hobbies.night.happy }}</h2>
+  <h2 v-show="infos.hobbies.night">During night, if I feel sad, I'm {{ infos.hobbies.night.sad }}</h2>
+  </div>
   <button @click="changeInfos">Change Infos</button>
+  <br>
+  <button @click="deleteHobby">Delete night hobby</button>
 </template>
 
 <script>
@@ -33,15 +37,16 @@ export default {
     function changeInfos() {
       // change value
       infos.name = "JJ"
-      // change nested value
-      infos.hobbies.day.push("Programming")
-      infos.hobbies.night.happy = "going to restaurant"
+    }
+    function deleteHobby(){
+      delete infos.hobbies.night
     }
 
     // return the data in a object
     return {
       infos,
       changeInfos,
+      deleteHobby
     };
   },
 };
