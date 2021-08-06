b4db5b85b4960ad75477f3435ba969a2eda7a6a7
angsuo <angsuo@13l>
2021-08-06 12:19:19 +0200

    ref function is to declare values to watch

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,59 @@
 <template>
-  <h1>Hello Vue 3</h1>
+  <h1>Ref usage in Vue 3</h1>
   <p>My name is {{name}}</p>
   <p>I'm {{age}} years old</p>
-  <p>My sex is: {{sex}}</p>
-  <p>The winner is: {{compet}}</p>
-  <button @click="sayHello">Say Hello</button>
-  <button @click="sayWelcome">Say Hello(Vue 2)</button>
-  <hr>
-  <button @click="showV3Data">Show Vue 3 data with Vue 2 methods</button>
-  <hr>
-  <button @click="showV2Data">Show Vue 2 data with Vue 3 methods</button>
-  <hr>
-  <button @click="showV3This">Show "this" in setup()</button>
+  <h2>During day, I like to:</h2>
+  <ul>
+    <li v-for="(hd, index) in hobbies.day" :key="index">{{hd}}</li>
+  </ul>
+  <h2>During night, if I feel happy, I'm {{hobbies.night.happy}}</h2 >
+  <h2>During night, if I feel sad, I'm {{hobbies.night.sad}}</h2 >
+  <button @click="showInfos">Show Infos</button>
+  <button @click="changeInfos">Change Infos</button>
 </template>
 
 <script>
-// if need to use "createElement"
-// import {h} from "vue"
-
+import {ref} from "vue"
 export default {
   name: 'App',
-  // vue 2 properties still work
-  data(){
-    return {
-      sex: "Unsexed",
-      compet:"V2"
-      }
-  },
-  methods: {
-    sayWelcome(){
-      alert("Welcome")
-    },
-    showV3Data(){
-      console.log("Sex:", this.sex)
-      console.log("name:", this.name)
-      this.sayHello()
-    }
-  },
-  // everything in vue 2 is returned in setup
   setup() {
-    // data
-    let name = "J"
-    let age = 18
-    // duplicate with Vue2's data(): the setup's value will be taken
-    let compet = "V3"
+    // // fixed data
+    // let name = "J"
+    // let age = 18
 
-    // methods
-    function sayHello(){
-      alert(`My name is ${name}, I'm ${age} yo`)
+    // declare referenced data
+// returns a new RefImpl object for simple values
+    let name = ref("J") 
+    let age = ref(18)
+    // returns a reactive object (Proxy) for objects
+    let hobbies = ref({ // r
+      day:["food", "sport"],
+      night: {
+        happy: "drinking",
+        sad: "sleeping"
+      }
+    })
+
+    // declare method
+    function showInfos(){
+      console.log("name:", name)
+      console.log("hobbies:", hobbies)
+      console.log("hobbies.day", hobbies.value.day)
     }
-
-
-    function showV2Data(){
-      console.log("sex:", this.sex) // undefined
-      this.sayWelcome() // error
+    function changeInfos(){
+      name.value = "JJ"
+      age.value ++
+      // don't know how to change referenced object yet
     }
-
-    function showV3This(){console.log(this)}
 
     // return the data in a object
     return {
       name,
       age,
-      compet, // linting error for duplicate declaration
-      sayHello,
-      showV2Data,
-      showV3This
+      hobbies,
+      showInfos,
+      changeInfos
     }
-
-    // return a callback function (h from vue 2)
-    // return () => h("h1", "I'm feeling alone")
   }
 }
 </script>
