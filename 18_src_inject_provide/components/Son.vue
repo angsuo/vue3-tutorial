8ec075b618afa0c59d4720fe79b49555054b7cfd
angsuo <angsuo91@gmail.com>
2021-08-08 11:57:38 +0200

    provide let parent component to pass reactive data to all layers of child components

---
+++ 18_src_inject_provide/components/Son.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>I'm the Son component</h2>
+  <p>My parent wants my child to do {{todo}}</p>
+    <hr>
+    <GrandSon/>
+</template>
+
+<script>
+import { inject } from '@vue/runtime-core'
+import GrandSon from "./GrandSon.vue"
+export default {
+    name:"Son",
+    components: {GrandSon},
+    setup() {
+        return {
+            todo: inject("todo")
+        }
+    }
+}
+</script>
